5c7ed7a44976a7bdac96b7a23636fa544bb22bb6
Mithil2603 <mithilsuthar2603@gmail.com>
2025-01-12 11:25:30 +0530

    admin name added

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./styles/Admin.css";
+import axios from "axios";
 
 export default function Admin() {
+  const [name, setName] = useState("");
 
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/")
+      .then((res) => {
+        if (res.data.status === "Success") {
+          setName(res.data.name);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <>
       <div className="container-fluid pt-5 custom-bg-admin">
         <div className="container w-100">
-          <button
-            className="btn custom-btn d-flex justify-content-center ms-auto fw-bolder"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasScrolling"
-            aria-controls="offcanvasScrolling"
-          >
-            Open Panel
-          </button>
+          <div className="d-flex justify-content-between align-items-center fw-bolder gap-3 shiny-box flex-wrap">
+            <div className="welcome text-dark">Welcome {name}!</div>
+            <button
+              className="btn custom-btn"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasScrolling"
+              aria-controls="offcanvasScrolling"
+            >
+              Open Panel
+            </button>
+          </div>
 
           <div
             className="offcanvas offcanvas-start"
